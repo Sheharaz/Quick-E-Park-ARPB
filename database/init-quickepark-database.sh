0e2009749bf2966284c8fb7168a14cea152b55fa
DESKTOP-E82C2M6\sheha <sheharaz07@gmail.com>
2021-05-05 11:59:15 +0200

    first

---
+++ database/init-quickepark-database.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;
+	ALTER ROLE postgres WITH PASSWORD 'Password';
+EOSQL
